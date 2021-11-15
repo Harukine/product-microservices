adf5b221b1f2183d43da1eb448c504ded39d0df9
Jacob <jacobhtnguyen@gmail.com>
2021-11-15 03:02:13 -0500

    finished implementing product service

---
+++ microservices/product-service/src/main/java/com/harukine/microservices/core/product/services/ProductServiceImpl.java
@@ -0,0 +1,38 @@
+package com.harukine.microservices.core.product.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import com.harukine.api.core.product.Product;
+import com.harukine.api.core.product.ProductService;
+import com.harukine.api.exceptions.InvalidInputException;
+import com.harukine.api.exceptions.NotFoundException;
+import com.harukine.util.http.ServiceUtil;
+
+@RestController
+public class ProductServiceImpl implements ProductService {
+    private static final Logger LOG = LoggerFactory.getLogger(ProductServiceImpl.class);
+
+    private final ServiceUtil serviceUtil;
+
+    @Autowired
+    public ProductServiceImpl(ServiceUtil serviceUtil) {
+        this.serviceUtil = serviceUtil;
+    }
+
+    @Override
+    public Product getProduct(int productId) {
+        LOG.debug("/product return the found product for productId={}", productId);
+
+        if (productId < 1) {
+            throw new InvalidInputException("Invalid productId: " + productId);
+        }
+
+        if (productId == 13) {
+            throw new NotFoundException("No product found for productId: " + productId);
+        }
+
+        return new Product(productId, "name-" + productId, 123, serviceUtil.getServiceAddress());
+    }
+}
